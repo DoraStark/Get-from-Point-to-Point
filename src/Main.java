312cb9d19f80ebbc820cc17cd93f760cfb987400
DoraStark <doraschneesturmin@gmail.com>
2024-06-30 17:55:49 +0200

    2

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Чтение графа из файла
-        String filePath = "stations.txt"; // путь к вашему файлу
+
+        String filePath = "stations.txt";
         WeightedGraph graph = GraphReader.readGraphFromFile(filePath);
 
-        // Печать графа для проверки
+
         graph.printGraph();
 
-        // Нахождение и печать кратчайшего пути
+
         PathFinder.findAndPrintShortestPath(graph);
 
-        // Рассчитываем кратчайшие времена путешествия от станции "S Schöneweide Bhf (Berlin)"
+
         String startStation = "S Schöneweide Bhf (Berlin)";
         String[] targetStations = {
                 "60068201511", "60066102852", "60053301433", "60120003653"
@@ -25,7 +25,7 @@ public class Main {
         }
     }
 
-    // Вспомогательный метод для расчета времени путешествия
+
     private static int calculateTravelTime(WeightedGraph graph, List<String> path) {
         int totalTime = 0;
         if (path == null || path.size() < 2) return totalTime;
